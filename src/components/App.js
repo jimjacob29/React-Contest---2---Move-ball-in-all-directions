5fd2e50edea01fad56b1bfe5d3fa1d4d836f5958
JIMMY JACOB <68079806+jimjacob29@users.noreply.github.com>
2020-10-26 17:53:07 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -10,73 +10,78 @@ const App = () => {
     top: "0px"
   });
   const reset = () => {
+    console.log("Reset Game");
     setRenderBall(false);
-    setX(0);
-    setY(0);
-    let obj = {};
-    obj.left = "0px";
-    obj.top = "0px";
-    setBallPosition(obj);
+    setBallPosition({
+      left: "0px",
+      top: "0px"
+    });
   };
-
-  const buttonStartClickHandler = () => {
+  const start = () => {
+    console.log("Start Game");
     setRenderBall(true);
   };
+  const renderChoice = () => {};
 
-  const renderChoice = () => {
-    if (renderBall) {
-      return <div className="ball" style={ballPosition}></div>;
-    } else
-      return (
-        <button className="start" onClick={buttonStartClickHandler}>
-          Start
-        </button>
-      );
-  };
-  const keyPressfn = (event) => {
-    if ((event.key === "ArrowRight" || event.keyCode === 39) && renderBall) {
-      const temp = x + 1;
-      setX(temp);
-      let obj = { ...ballPosition };
-      obj.left = temp * 5 + "px";
-      setBallPosition(obj);
+  const handleMovement1 = (event) => {
+    if (
+      event.keyCode !== 37 &&
+      event.keyCode !== 38 &&
+      event.keyCode !== 39 &&
+      event.keyCode !== 40
+    )
+      return;
+    console.log("keyPress :", event.key);
+    console.log("initial pos=", ballPosition);
+    let leftPos = parseInt(ballPosition.left.split("px")[0]);
+    let topPos = parseInt(ballPosition.top.split("px")[0]);
+
+    switch (event.keyCode) {
+      case 37:
+        leftPos -= 5;
+        break;
+      case 38:
+        topPos -= 5;
+        break;
+      case 39:
+        leftPos += 5;
+        break;
+      case 40:
+        topPos += 5;
+        break;
+      default:
+        break;
     }
-    if ((event.key === "ArrowLeft" || event.keyCode === 37) && renderBall) {
-      const temp = x - 1;
-      setX(temp);
-      let obj = { ...ballPosition };
-      obj.left = temp * 5 + "px";
-      setBallPosition(obj);
-    }
-    if ((event.key === "ArrowUp" || event.keyCode === 38) && renderBall) {
-      const temp = y - 1;
-      setY(temp);
-      let obj = { ...ballPosition };
-      obj.top = temp * 5 + "px";
-      setBallPosition(obj);
-    }
-    if ((event.key === "ArrowDown" || event.keyCode === 40) && renderBall) {
-      const temp = y + 1;
-      setY(temp);
-      let obj = { ...ballPosition };
-      obj.top = temp * 5 + "px";
-      setBallPosition(obj);
-    }
+
+    const newPos = {
+      left: `${leftPos}px`,
+      top: `${topPos}px`
+    };
+    console.log("final pos=", newPos);
+    setBallPosition(newPos);
   };
 
   useEffect(() => {
-    document.addEventListener("keydown", keyPressfn);
+    document.addEventListener("keydown", handleMovement1);
+    console.log("Listner added");
 
     return () => {
-      document.removeEventListener("keydown", keyPressfn);
+      document.removeEventListener("keydown", handleMovement1);
     };
   });
-
   return (
     <div className="playground">
+      {renderBall === false ? (
+        <button onClick={start} className="start">
+          Start
+        </button>
+      ) : (
+        <div className="ball" style={ballPosition}></div>
+      )}
       <button onClick={reset} className="reset">
         Reset
       </button>
+
       {renderChoice()}
     </div>
   );
